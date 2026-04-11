c4b74c192634809ba6036b2f5155ba9f5c041012
Jasmitsingh01 <jasmits007@gmail.com>
2026-04-11 12:37:24 +0530

    feat: add custom production error view template

---
+++ app/Views/errors/html/error_production.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Production Error</title>
+</head>
+<body>
+    <h1>Something went wrong.</h1>
+    <p>Please check the application logs for more information.</p>
+</body>
+</html>
